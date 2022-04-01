c2f9ee583c2d4c40c1c8f2b8a885a697b88c1ba0
GastonOdetti <gastonodetti@gmail.com>
2022-04-01 15:42:05 -0300

    localstorage

--- JS projects/Cirriculum Vitae/index.js
+++
@@ -1,7 +0,0 @@
-function Presentation() {
-    return (<h2>Its a me, Gaston!</h2>);
-  }
-
-
-
-ReactDOM.render(<Presentation />, document.getElementById("presentation"))
--- JS projects/To Do List/index.js
+++ JS projects/To Do List/index.js
@@ -1,55 +1,58 @@
-//localstorage ver
+const tareasLista = document.getElementById("ul")
+const ingreso = document.getElementById("input")
+const boton = document.getElementById("button")
+const botonBorrar = document.getElementById("borrar")
+let arrayTareas = JSON.parse(localStorage.getItem('tareasGuardadas'))
 
-var ul = document.getElementById("ul")
-var myArray = []
-var i = 0;
-
-function addTask() {
-
-  
-  let textToInput = document.getElementById("input").value
-  myArray.push(textToInput)
-
-  while (i < myArray.length){
-
-    if (textToInput !== "") {
-
-      let li = document.createElement("li")
-      li.className = "liClass"
-  
-      let input = document.createElement("button")
-      input.innerText = "x"
-      input.className ="checkbox"
-      
-      li.innerText = myArray[i]
-      console.log(myArray[i])
-    
-      li.appendChild(input)
-      ul.appendChild(li)
-      i++
-    }
-} }
+function recuperarTareas(){
+  if(!arrayTareas){
+    arrayTareas = []
+  } 
+}
 
 
-// Add a "checked" symbol when clicking on a list item - copied from https://www.w3schools.com/howto/howto_js_todolist.asp
-var list = document.querySelector('ul');
+function renderTareas(){
 
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-   
+  tareasLista.innerHTML = ""
+
+  arrayTareas.forEach((element,i) => {
+    let li = document.createElement("li")
+    li.className = "liClass"
+    li.id = i
+    li.innerText = element
+
+    tareasLista.appendChild(li)
+  });
+}
+
+boton.addEventListener("click", function(){
+
+  if (ingreso.value.length > 2 ){
+    arrayTareas.push(ingreso.value)
   }
-}, false);
+
+  localStorage.setItem('tareasGuardadas', JSON.stringify(arrayTareas))
+
+  renderTareas()
+
+})
 
 
-//close button for tasks
 
-list.addEventListener('click', function(ev) {
-  
-  console.log()
+botonBorrar.addEventListener("click", function(){
+  localStorage.removeItem('tareasGuardadas')
+  tareasLista.innerHTML = ""
+})
 
-  if (ev.target.className === 'checkbox') {
-   console.log("test")   
-   
-  }
-}, false);
+
+recuperarTareas()
+renderTareas()
+
+
+let lis = document.getElementsByTagName('li');
+
+[].forEach.call(lis, element => {
+  element.addEventListener('click', () => {
+     element.className = "checked"
+  });
+});
