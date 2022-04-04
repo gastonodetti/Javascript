684e7ee8d8a4bc299cd64c663cce9e86ebc74748
GastonOdetti <gastonodetti@gmail.com>
2022-04-04 15:49:35 -0300

    to-do avances

--- JS projects/To Do List/index.js
+++ JS projects/To Do List/index.js
@@ -10,49 +10,51 @@ function recuperarTareas(){
   } 
 }
 
-
 function renderTareas(){
 
   tareasLista.innerHTML = ""
 
   arrayTareas.forEach((element,i) => {
+
     let li = document.createElement("li")
     li.className = "liClass"
     li.id = i
     li.innerText = element
 
     tareasLista.appendChild(li)
+
+
   });
 }
 
 boton.addEventListener("click", function(){
 
-  if (ingreso.value.length > 2 ){
+  if (ingreso.value.length > 1 ){
     arrayTareas.push(ingreso.value)
   }
 
   localStorage.setItem('tareasGuardadas', JSON.stringify(arrayTareas))
 
-  renderTareas()
+  renderTareas("liClass")
+  ingreso.value = ''
 
 })
 
-
-
 botonBorrar.addEventListener("click", function(){
+  arrayTareas = []
   localStorage.removeItem('tareasGuardadas')
   tareasLista.innerHTML = ""
 })
-
 
 recuperarTareas()
 renderTareas()
 
 
-let lis = document.getElementsByTagName('li');
+tareasLista.addEventListener('click', function(event) {
+  if (event.target.tagName === 'LI') {
+    event.target.classList.toggle('checked'); 
+  }
 
-[].forEach.call(lis, element => {
-  element.addEventListener('click', () => {
-     element.className = "checked"
-  });
-});
+}, false);
+
+
